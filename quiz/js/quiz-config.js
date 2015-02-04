d61ab347d839b635575e4ad1c733946a87aa906c
PhilipTse <PhilipTse@users.noreply.github.com>
2015-02-04 13:06:10 -0800

    Update quiz-config.js

--- quiz/js/quiz-config.js
+++ quiz/js/quiz-config.js
@@ -4,9 +4,9 @@
 
 var quizJSON = {
     "info": {
-        "name":    "Into Tech Quiz!",
+        "name":    "Into Tech Quiz!!",
         "main":    "<p>Think you're Into Tech? Find out with this super crazy knowledge quiz!</p>",
-        "results": "<p>Great Job! Click go back to go back to the main page.</p><br><a class=\"button\" href=\"www.into-tech.com\">Go back!</a><br>",
+        "results": "<p>Great Job! Click go back to go back to the main page.</p><br><a class=\"button\" href=\"www.into-tech.com\">Go back!</a><br>&nbsp;&nbsp;&nbsp;&nbsp;",
         "level1":  "Into Tech Master!",
         "level2":  "Into Tech Pro!",
         "level3":  "Into Tech Amateur",
